0f16ce16e7f708f74661ac1fc5a034a7b7617b49
sprit-maberasu-1140-source <>
2026-05-22 06:48:21 +0000

    Challenge: Generate Social Media Post Templates

---
+++ automating_content_workflows/challenge_generate_social_media_post_templates/main.py
@@ -0,0 +1,13 @@
+titles = [
+    "10 Tips for Better Photography",
+    "Mastering Video Editing in 2024",
+    "Content Planning for Busy Creators",
+    "How to Grow Your Audience Organically"
+]
+
+def generate_post_templates(titles):
+    for title in titles:
+        post = f"Check out our new post: {title}! #ContentCreator"
+        print(post)
+
+generate_post_templates(titles)
